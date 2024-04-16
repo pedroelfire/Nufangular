de5c3c2eb9ebae3eb18c62a1b833965f3f5f116f
EdgarQuinonez <l22410043@itparral.edu.mx>
2024-04-16 12:24:53 -0600

    Login primeng block dependencies imported in login module

---
+++ src/app/login/feature/login/login.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms'; // Needed for input and [(ngModel)] usage
+
+// Import PrimeNG Modules 
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+      CommonModule,
+      FormsModule, // For forms
+      InputTextModule,
+      CheckboxModule,
+      ButtonModule,
+      RippleModule 
+    
+  ]
+})
+export class LoginModule { }
