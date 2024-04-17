bfb85902554e31f6cef8849f8a257e0110b244b1
EdgarGlowbo <efqr811@gmail.com>
2024-04-16 21:55:39 -0600

    primeng downgrade. bootstrap 4.6. login module

--- src/app/login/feature/login/login.component.ts
+++ src/app/login/feature/login/login.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  showError = false;
 
+  logIn = () => {
+    const success = false;
+
+    if (success) {
+      console.log('Ingresado con éxito');
+    } else {
+      this.showError = true;
+      console.log('toast shown');
+    }
+  };
 }
--- src/app/login/feature/login/login.module.ts
+++ src/app/login/feature/login/login.module.ts
@@ -1,30 +1,29 @@
 import { NgModule } from '@angular/core';
+import { ToastModule } from 'primeng/toast';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms'; // Needed for input and [(ngModel)] usage
 
-// Import PrimeNG Modules 
+// Import PrimeNG Modules
 import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-      CommonModule,
-      FormsModule, // For forms
-      InputTextModule,
-      CheckboxModule,
-      ButtonModule,
-      RippleModule 
-    
-  ]
+    CommonModule,
+    FormsModule, // For forms
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+    RippleModule,
+    ToastModule,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
