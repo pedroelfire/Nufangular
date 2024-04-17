23bba916665e5b08dbf65a2ca95e8a9c09235767
pedroelfire <pedroelfire@gmail.com>
2024-04-17 12:57:11 -0600

    Mas avances pero poquitos andamos valiendo verga como todos los dias

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -9,15 +9,7 @@ export class LoginService {
   private apiUrl = 'http://127.0.0.1:8000/user/auth/';
 
   constructor(private http: HttpClient) { }
-
   login(username: string, password: string): Observable<any> {
-    // Obtener el token CSRF de la cookie
-    // Construir el encabezado con el token CSRF
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-    });
-
-    // Realizar la solicitud POST con el encabezado personalizado
-    return this.http.post<any>(this.apiUrl, { username, password }, { headers });
+    return this.http.post<any>(this.apiUrl, { username, password });
   }
 }
