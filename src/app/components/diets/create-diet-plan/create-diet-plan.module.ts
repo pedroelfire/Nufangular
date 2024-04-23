45d3945671297f1736c2d0889e257cab9e00d1d2
EdgarQuinonez <l22410043@itparral.edu.mx>
2024-04-22 20:03:06 -0600

    angular-calendar implemented

--- src/app/components/diets/create-diet-plan/create-diet-plan.module.ts
+++ src/app/components/diets/create-diet-plan/create-diet-plan.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CreateDietPlanComponent } from './create-diet-plan.component';
 import { MealItineraryComponent } from '../meal-itinerary/meal-itinerary.component';
+import { CalendarComponent } from '../calendar/calendar.component';
 
 @NgModule({
-  declarations: [CreateDietPlanComponent, MealItineraryComponent],
-  imports: [CommonModule],
+  declarations: [
+    CreateDietPlanComponent,
+    MealItineraryComponent,
+    CalendarComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
 })
 export class CreateDietPlanModule {}
