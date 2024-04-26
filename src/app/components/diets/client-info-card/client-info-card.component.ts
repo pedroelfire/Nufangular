a58e7ed24f8a033aa2f3dc0ef65ced4840ee2a1f
EdgarQuinonez <l22410043@itparral.edu.mx>
2024-04-25 20:24:09 -0600

    Layout de create-diet-plan terminado

--- src/app/components/diets/client-info-card/client-info-card.component.ts
+++ src/app/components/diets/client-info-card/client-info-card.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {
+  faPersonRunning,
+  faBullseye,
+  faNotesMedical,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-client-info-card',
   templateUrl: './client-info-card.component.html',
-  styleUrls: ['./client-info-card.component.scss']
+  styleUrls: ['./client-info-card.component.scss'],
 })
 export class ClientInfoCardComponent {
-
+  // FontAwesome Icons
+  faPersonRunning = faPersonRunning;
+  faBullseye = faBullseye;
+  faNotesMedical = faNotesMedical;
 }
--- src/app/components/diets/client-info-card/client-info-card.module.ts
+++ src/app/components/diets/client-info-card/client-info-card.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClientInfoCardComponent } from './client-info-card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [ClientInfoCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [ClientInfoCardComponent],
 })
 export class ClientInfoCardModule {}
