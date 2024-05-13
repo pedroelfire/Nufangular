67bd2d32d914c67f5d193344f23a8e535e35f73c
EdgarQuinonez <l22410043@itparral.edu.mx>
2024-05-12 18:23:53 -0600

    audio recording not working

---
+++ src/app/services/audio-recording.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { bufferToWave } from '../utils/audio-helper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AudioRecordingService {
+  private chunks: any[] = [];
+  private mediaRecorder: any;
+  private audioContext: AudioContext = new AudioContext();
+  private audioBlobSubject = new Subject<Blob>();
+
+  audioBlob$ = this.audioBlobSubject.asObservable();
+
+  async startRecording() {
+    if (this.audioContext.state === 'suspended') {
+      await this.audioContext.resume();
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    this.mediaRecorder = new MediaRecorder(stream);
+    this.mediaRecorder.ondataavailable = (event: any) =>
+      this.chunks.push(event.data);
+    this.mediaRecorder.start();
+  }
+
+  async stopRecording() {
+    if (this.mediaRecorder) {
+      this.mediaRecorder.onstop = async () => {
+        const audioBlob = await new Blob(this.chunks);
+        // const audioData = await new Blob(this.chunks).arrayBuffer();
+        // const audioBuffer = await this.audioContext.decodeAudioData(audioData);
+        // const wavBlob = bufferToWave(audioBuffer, audioBuffer.length);
+        this.audioBlobSubject.next(audioBlob);
+        this.chunks = [];
+      };
+
+      this.mediaRecorder.stop();
+    }
+  }
+}
--- src/app/shared/jack/jack-message-composer/jack-message-composer.component.ts
+++ src/app/shared/jack/jack-message-composer/jack-message-composer.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { AudioRecordingService } from 'src/app/services/audio-recording.service';
 
 @Component({
   selector: 'app-jack-message-composer',
@@ -10,8 +12,12 @@ export class JackMessageComposerComponent {
   @Output() newMessageEvent = new EventEmitter<string>();
 
   messageForm: FormGroup;
+  isRecording = false;
+  audioBlob: Blob | null = null;
+  audioUrl: string | null = null;
+  private subscription!: Subscription;
 
-  constructor() {
+  constructor(private audioRecordingService: AudioRecordingService) {
     this.messageForm = new FormGroup({
       message: new FormControl('', [
         Validators.required,
@@ -20,10 +26,35 @@ export class JackMessageComposerComponent {
     });
   }
 
-  isRecording = false;
+  ngOnInit() {
+    this.subscription = this.audioRecordingService.audioBlob$.subscribe(
+      (blob) => {
+        this.audioBlob = blob;
+        this.audioUrl = URL.createObjectURL(blob); // Create URL for audio playback
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  handleMicClick() {
+    if (this.isRecording) {
+      this.stopRecording();
+    } else {
+      this.startRecording();
+    }
+  }
 
   startRecording() {
     this.isRecording = true;
+    this.audioRecordingService.startRecording();
+  }
+
+  stopRecording() {
+    this.isRecording = false;
+    this.audioRecordingService.stopRecording();
   }
 
   submitMessage() {
