30787132243b366fb7c904d54b67aa97f91a85d1
EdgarQuinonez <l22410043@itparral.edu.mx>
2024-05-13 16:39:43 -0600

    jack side menu and recording animations

--- src/app/shared/jack/jack-side-bar-menu/jack-side-bar-menu.component.ts
+++ src/app/shared/jack/jack-side-bar-menu/jack-side-bar-menu.component.ts
@@ -1,9 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-jack-side-bar-menu',
   templateUrl: './jack-side-bar-menu.component.html',
   styleUrls: ['./jack-side-bar-menu.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('300ms ease-in-out', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in-out', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class JackSideBarMenuComponent {
   @Input() displayed!: boolean;
